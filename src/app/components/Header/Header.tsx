d0397e4b51a858e4a7affaa61001acd0bf876d56
wiseman88 <wiseadam02@gmail.com>
2023-06-12 18:38:55 +0200

    style: header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import styles from './header.module.scss'
 
 const Header = () => {
-  return <header>here goes header</header>
+  return (
+    <header className={styles.header__main}>
+      <div className={styles.logo}>
+        <a href="">movea</a>
+      </div>
+      <nav className={styles.navigation}>
+        <ul role="menubar">
+          <li role="none">
+            <a href="" role="menuitem">
+              Movies
+            </a>
+          </li>
+          <li role="none">
+            <a href="" role="menuitem">
+              Tv shows
+            </a>
+          </li>
+          <li role="none">
+            <a href="" role="menuitem">
+              Animations
+            </a>
+          </li>
+          <li role="none">
+            <a href="" role="menuitem">
+              Plans
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li role="none">
+            <a href="" role="menuitem">
+              Search
+            </a>
+          </li>
+          <li role="none">
+            <a href="" role="menuitem">
+              Online
+            </a>
+          </li>
+          <li role="none">
+            <a href="" role="menuitem">
+              Icon
+            </a>
+          </li>
+          <li role="none">
+            <a href="" role="menuitem">
+              Avatar
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
